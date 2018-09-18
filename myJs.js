9641f5ebe26d0869742486e29c644b1878c39ae0
VadymBoguslavsky <axixe92@gmail.com>
2018-09-18 21:48:39 +0300

    sider

---
+++ myJs.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+ $('.main_slider').slick({
+  nextArrow: '<i class="fa fa-arrow-right arrow_right"></i>',
+  prevArrow: '<i class="fa fa-arrow-left arrow_left"></i>',
+  asNavFor: '.slider2',
+  infinite: true,
+  draggable: true,
+  responsive: [
+   {
+    breakpoint: 1000,
+    settings: {
+     dots: true
+    }
+   }
+  ]
+ });
+
+ $('.slider2').slick({
+  infinite: true,
+  slidesToShow: 5,
+  focusOnSelect: true,
+  centerMode: true,
+  asNavFor: '.main_slider',
+  appendArrows: false
+ });
+});
